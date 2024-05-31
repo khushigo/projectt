79e9eb362ee757d8ccc8d904bcfe498d07bc74d7
khushigo <khushi74@gmail.com>
2024-05-31 12:18:13 +0530

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div className='about' >
+      <center style={{backgroundColor:'skyblue',backgroundImage:"url('https://th.bing.com/th/id/OIP.FBDSPOAoqyU5hyr96dfwCQHaE8?rs=1&pid=ImgDetMain')",width:'1500px' ,height:'1000px'}}>
+      <h1 style={{color:'red', fontSize:'50px'}}>About Page</h1>
+      
+      </center>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className='pro'><center style={{backgroundColor:'black', backgroundImage:"url('https://th.bing.com/th/id/OIP.lDlGFX-2mRfBe-Ucwr2UcQHaFN?rs=1&pid=ImgDetMain')",width:'1600px' ,height:'1500px' }}>
+      <h1 style={{color:'white', fontSize:'50px'}}>Welcome to our page</h1>
+       
+      </center>
+    </div>
+  );
+}
+
+export default Home;
